4fb8a76b6dbd1318602d5a3d53984d66c95b938e
Patrick Harned <pat@li-819025cc-34a5-11b2-a85c-ba699012569d.ibm.com>
2023-03-12 19:42:18 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::io;
+use std::io::{Read, Write};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+
+    write(buffer)
+}
+
+
+fn e_cut(window_1_size: i32, window_2_size:i32) -> f32{
+    let d_confidence: f32 = 0.1;
+
+    let m:f32 = 1 as f32/((1 as f32 / window_1_size as f32) as f32 + (1 as f32 / window_2_size as f32 ) as f32);
+
+    let delta:f32 = d_confidence / 100 as f32;
+
+    let e_cut: f32 = (1 as f32/(2 as f32 *m))* (4 as f32 /delta as f32).log(10 as f32);
+
+    e_cut.sqrt()
+}
+
+
+
+fn write(buf:String) -> io::Result<()> {
+
+    let mut vec: Vec<i32> = vec![];
+    let mut vec2: Vec<i32> = vec![];
+    let window_size :i32 = 10;
+
+    for i in buf.trim().split('\n') {
+
+        let x: i32 = i.trim().parse().expect("Input not an integer");
+
+
+        if vec.len()as i32 > window_size{
+            vec2.push(x);
+
+            if vec2.len() as i32 == window_size{
+                if (vec.iter().sum::<i32>() as f32 / vec.len() as f32) - (vec2.iter().sum::<i32>() as f32 / vec.len() as f32) >e_cut(vec.len() as i32, vec2.len() as i32) {
+                    let out: String = "Found drift at ".to_owned() + &x.to_string() + "\n";
+                    io::stdout().write(out.as_ref())?;
+                    vec.clear();
+                    vec2 = Vec::new();
+
+                }
+
+            }
+        }
+        vec.push(x);
+    }
+    Ok(())
+}
