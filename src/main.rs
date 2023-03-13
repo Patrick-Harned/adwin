24b3a5d2dde262f9864e29a461aef57f6ef777df
Patrick Harned <pat@li-819025cc-34a5-11b2-a85c-ba699012569d.ibm.com>
2023-03-12 23:12:29 -0500

    empt

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> io::Result<()> {
 
 
 fn e_cut(window_1_size: i32, window_2_size:i32) -> f32{
-    let d_confidence: f32 = 0.1;
+    let d_confidence: f32 = 0.9;
 
     let m:f32 = 1 as f32/((1 as f32 / window_1_size as f32) as f32 + (1 as f32 / window_2_size as f32 ) as f32);
 
@@ -40,17 +40,15 @@ fn write(buf:String) -> io::Result<()> {
             vec2.push(x);
 
             let vec1_avg = vec.iter().sum::<i32>() as f32 / vec.len() as f32;
-            let vec2_avg = vec2.iter().sum::<i32>() as f32 / vec.len() as f32;
+            let vec2_avg = vec2.iter().sum::<i32>() as f32 / vec2.len() as f32;
 
-                if (vec1_avg -vec2_avg) > e_cut(vec.len() as i32, vec2.len() as i32) {
-                    let out: String = "Found drift at ".to_owned() + &x.to_string() + "\n";
+                if (vec1_avg -vec2_avg).abs() > e_cut(vec.len() as i32, vec2.len() as i32) {
+                    let out: String = "Found drift at ".to_owned() + &x.to_string() + " with first window average " + &vec1_avg.to_string() + " and second window average " + &vec2_avg.to_string() +  "\n";
                     io::stdout().write(out.as_ref())?;
-                    vec.clear();
+                    vec = Vec::new();
                     vec2 = Vec::new();
 
                 }
-
-
 
         }
         vec.push(x);
